bff750ca08e7b6bfee124ecbe23c943eb809d527
Golnar Mordadi <golnarmordadi@gmail.com>
2023-03-01 21:08:23 +0330

    📝 Added Comments

--- view/main.go
+++ view/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hakimehmordadi/GOlang-Restful/controller"
+	"github.com/hakimehmordadi/GOlang-Restful/api/controller"
 )
 
 type File struct {
@@ -14,12 +14,15 @@ type File struct {
 }
 
 func StartServer() {
+	// Create a router using Gin framework
 	router := gin.Default()
+	// Grouping a bunch of same endpoints
 	api := router.Group("/api")
 	v1 := api.Group("/v1")
 	files := v1.Group("/files")
 
 	files.POST("/", func(c *gin.Context) {
+		// retrieves a single file from the request
 		file, err := c.FormFile("file")
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -38,6 +41,7 @@ func StartServer() {
 
 	files.GET("/:name/", func(c *gin.Context) {
 		var f File
+		// Bind url variables
 		if err := c.ShouldBindUri(&f); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err})
 			return
@@ -48,6 +52,7 @@ func StartServer() {
 			return
 		}
 		c.Header("Content-Disposition", "attachment; filename="+f.Name)
+		// Server the file
 		c.Data(http.StatusOK, m, cn)
 	})
 	_ = router.Run(":8080")
